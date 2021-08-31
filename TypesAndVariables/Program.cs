8e79b33ab5c6a3b7bc920926a6a09b68b0d7b73d
rekcahi <aydogdurecai@gmail.com>
2021-08-31 23:59:16 +0300

    test2

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -15,8 +15,17 @@ namespace TypesAndVariables
             short numberShort = 23;//           
             int number1 = 12;
             long numberLong = 23;
+            var sayi = 2;
+            sayi = 'A';
             Console.WriteLine("Char {0}",charakter);
+            Console.WriteLine(Days.Friday);
+            Console.WriteLine(sayi);
             Console.ReadLine();
+        }
+        enum Days
+        {
+            Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+
         }
     }
 }
