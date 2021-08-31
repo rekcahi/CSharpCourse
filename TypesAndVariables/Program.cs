b17b235af08c3c9c8209f7ae992dd0eefcfa3c55
rekcahi <aydogdurecai@gmail.com>
2021-08-31 23:49:37 +0300

    ilk commit

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double doubleSayi = 10.4;
+            decimal decimalSayi = 10.5m;
+            char charakter = 'A';
+            string charakterDizisi = "Ankara";
+            bool condition = true;
+            byte numberByte = 255;
+            short numberShort = 23;//           
+            int number1 = 12;
+            long numberLong = 23;
+            Console.WriteLine("Char {0}",charakter);
+            Console.ReadLine();
+        }
+    }
+}
