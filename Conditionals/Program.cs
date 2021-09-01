2a5bb4f280d834ec64dbe3c2e37967ac75bead6a
rekcahi <aydogdurecai@gmail.com>
2021-09-01 23:07:49 +0300

    şartlar

---
+++ Conditionals/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+            Console.WriteLine(number==10?"Number is 10":"Number is not 10");
+        }
+    }
+}
