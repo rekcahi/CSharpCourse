33cff60b83a29cf0f003992ee5f123b36d613a1a
rekcahi <aydogdurecai@gmail.com>
2021-09-04 23:18:58 +0300

    Loops Nad Array Added

---
+++ Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studensts = { "Recai", "Ali", "Veli" };
+
+            int[,] dizi2d = new int[2, 3];
+
+            dizi2d[0, 0] = 1;
+            dizi2d[0, 1] = 2;
+            dizi2d[0, 2] = 3;
+
+            dizi2d[1, 0] = 4;
+            dizi2d[1, 1] = 5;
+            dizi2d[1, 2] = 6;
+            for (int i = 0; i < dizi2d.GetLength(0); i++)
+            {
+                for (int j = 0; j < dizi2d.GetLength(1); j++)
+                {
+                    Console.WriteLine( dizi2d[i,j] );
+
+                }
+                Console.WriteLine("**********");
+
+            }
+
+
+            //foreach (var item in studensts)
+            //{
+            //    Console.WriteLine(item);
+
+            //}
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ForMethod();
+            // WhileMethod();
+            // DoWhileMethod();
+            // ForeachMethod();
+          bool result= IsPrimeNumber(5);
+            Console.WriteLine("Sonuç {0}",result);
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number; i++)
+            {
+                Console.WriteLine(i);
+                if (number % i == 0)
+                {
+                    result = false;
+                    break;
+                
+                }
+
+            }
+            return result;
+        }
+
+        private static void ForeachMethod()
+        {
+            string[] kisiler = { "Ali", "Veli", "Deli" };
+            foreach (var item in kisiler)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+
+        private static void DoWhileMethod()
+        {
+            int number = 100;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+
+            } while (number>0);
+        }
+
+        private static void WhileMethod()
+        {
+            int number = 100;
+
+            while (number > 0)
+            {
+                Console.WriteLine(number);
+                number--;
+
+            }
+        }
+
+        private static void ForMethod()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+
+            }
+        }
+    }
+}
